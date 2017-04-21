85b0496f40fbe0a58f93dd54f136799376cceb3a
tmburt04 <tmburt04@cardmail.louisville.edu>
2017-04-21 11:50:56 -0400

    Initial creation

---
+++ Program 3.0/Prog2/RegForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Prog2
+{
+    partial class RegForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lastNameTxt = new System.Windows.Forms.TextBox();
+            this.findRegTimeBtn = new System.Windows.Forms.Button();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.RegLbl = new System.Windows.Forms.Label();
+            this.dateTimeLbl = new System.Windows.Forms.Label();
+            this.standingGrpBox = new System.Windows.Forms.GroupBox();
+            this.freshBtn = new System.Windows.Forms.RadioButton();
+            this.sophBtn = new System.Windows.Forms.RadioButton();
+            this.juniorBtn = new System.Windows.Forms.RadioButton();
+            this.seniorBtn = new System.Windows.Forms.RadioButton();
+            this.standingGrpBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lastNameTxt
+            // 
+            this.lastNameTxt.Location = new System.Drawing.Point(135, 29);
+            this.lastNameTxt.Name = "lastNameTxt";
+            this.lastNameTxt.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTxt.TabIndex = 1;
+            // 
+            // findRegTimeBtn
+            // 
+            this.findRegTimeBtn.Location = new System.Drawing.Point(74, 146);
+            this.findRegTimeBtn.Name = "findRegTimeBtn";
+            this.findRegTimeBtn.Size = new System.Drawing.Size(99, 23);
+            this.findRegTimeBtn.TabIndex = 2;
+            this.findRegTimeBtn.Text = "Find Reg. Time";
+            this.findRegTimeBtn.UseVisualStyleBackColor = true;
+            this.findRegTimeBtn.Click += new System.EventHandler(this.findRegTimeBtn_Click);
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(132, 13);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(59, 13);
+            this.nameLbl.TabIndex = 4;
+            this.nameLbl.Text = "Last name:";
+            // 
+            // RegLbl
+            // 
+            this.RegLbl.AutoSize = true;
+            this.RegLbl.Location = new System.Drawing.Point(47, 172);
+            this.RegLbl.Name = "RegLbl";
+            this.RegLbl.Size = new System.Drawing.Size(154, 13);
+            this.RegLbl.TabIndex = 5;
+            this.RegLbl.Text = "The earliest you may register is:";
+            // 
+            // dateTimeLbl
+            // 
+            this.dateTimeLbl.AutoSize = true;
+            this.dateTimeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimeLbl.Location = new System.Drawing.Point(47, 185);
+            this.dateTimeLbl.Name = "dateTimeLbl";
+            this.dateTimeLbl.Size = new System.Drawing.Size(0, 13);
+            this.dateTimeLbl.TabIndex = 6;
+            // 
+            // standingGrpBox
+            // 
+            this.standingGrpBox.Controls.Add(this.freshBtn);
+            this.standingGrpBox.Controls.Add(this.sophBtn);
+            this.standingGrpBox.Controls.Add(this.juniorBtn);
+            this.standingGrpBox.Controls.Add(this.seniorBtn);
+            this.standingGrpBox.Location = new System.Drawing.Point(13, 13);
+            this.standingGrpBox.Name = "standingGrpBox";
+            this.standingGrpBox.Size = new System.Drawing.Size(102, 117);
+            this.standingGrpBox.TabIndex = 0;
+            this.standingGrpBox.TabStop = false;
+            this.standingGrpBox.Text = "Class Standing";
+            // 
+            // freshBtn
+            // 
+            this.freshBtn.AutoSize = true;
+            this.freshBtn.Location = new System.Drawing.Point(6, 89);
+            this.freshBtn.Name = "freshBtn";
+            this.freshBtn.Size = new System.Drawing.Size(71, 17);
+            this.freshBtn.TabIndex = 3;
+            this.freshBtn.Text = "Freshman";
+            this.freshBtn.UseVisualStyleBackColor = true;
+            // 
+            // sophBtn
+            // 
+            this.sophBtn.AutoSize = true;
+            this.sophBtn.Location = new System.Drawing.Point(7, 66);
+            this.sophBtn.Name = "sophBtn";
+            this.sophBtn.Size = new System.Drawing.Size(79, 17);
+            this.sophBtn.TabIndex = 2;
+            this.sophBtn.Text = "Sophomore";
+            this.sophBtn.UseVisualStyleBackColor = true;
+            // 
+            // juniorBtn
+            // 
+            this.juniorBtn.AutoSize = true;
+            this.juniorBtn.Location = new System.Drawing.Point(7, 43);
+            this.juniorBtn.Name = "juniorBtn";
+            this.juniorBtn.Size = new System.Drawing.Size(53, 17);
+            this.juniorBtn.TabIndex = 1;
+            this.juniorBtn.Text = "Junior";
+            this.juniorBtn.UseVisualStyleBackColor = true;
+            // 
+            // seniorBtn
+            // 
+            this.seniorBtn.AutoSize = true;
+            this.seniorBtn.Checked = true;
+            this.seniorBtn.Location = new System.Drawing.Point(7, 20);
+            this.seniorBtn.Name = "seniorBtn";
+            this.seniorBtn.Size = new System.Drawing.Size(55, 17);
+            this.seniorBtn.TabIndex = 0;
+            this.seniorBtn.TabStop = true;
+            this.seniorBtn.Text = "Senior";
+            this.seniorBtn.UseVisualStyleBackColor = true;
+            // 
+            // RegForm
+            // 
+            this.AcceptButton = this.findRegTimeBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 209);
+            this.Controls.Add(this.standingGrpBox);
+            this.Controls.Add(this.dateTimeLbl);
+            this.Controls.Add(this.RegLbl);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.findRegTimeBtn);
+            this.Controls.Add(this.lastNameTxt);
+            this.Name = "RegForm";
+            this.Text = "Program 2";
+            this.standingGrpBox.ResumeLayout(false);
+            this.standingGrpBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lastNameTxt;
+        private System.Windows.Forms.Button findRegTimeBtn;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label RegLbl;
+        private System.Windows.Forms.Label dateTimeLbl;
+        private System.Windows.Forms.GroupBox standingGrpBox;
+        private System.Windows.Forms.RadioButton freshBtn;
+        private System.Windows.Forms.RadioButton sophBtn;
+        private System.Windows.Forms.RadioButton juniorBtn;
+        private System.Windows.Forms.RadioButton seniorBtn;
+    }
+}
+
---
+++ Program 3.0/Prog2/RegForm.cs
@@ -0,0 +1,134 @@
+﻿// Program 2
+// CIS 199-01/-75
+// Due: 4/07/2015
+// Tim Burton
+
+// This application calculates the earliest registration date
+// and time for an undergraduate student given their credit hours
+// and last name.
+// Decisions based on UofL Spring 2015 Priority Registration Schedule
+
+// Solution 3
+// This solution keeps the first letter of the last name as a char
+// and uses if/else logic for the times.
+// It uses defined strings for the dates and times to make it easier
+// to maintain.
+// It performs validation on the entered last name.
+// It only uses programming elements introduced in the text or
+// in class.
+// This solution takes advantage of the fact that there really are
+// only two different time patterns used. One for juniors and seniors
+// and one for sophomores and freshmen. The pattern for sophomores
+// and freshmen is complicated by the fact the certain letter ranges
+// get one date and other letter ranges get another date.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prog2
+{
+    public partial class RegForm : Form
+    {
+        public RegForm()
+        {
+            InitializeComponent();
+        }
+
+        private void findRegTimeBtn_Click(object sender, EventArgs e)
+        {
+            const string DAY1 = "April 1";  // 1st day of registration
+            const string DAY2 = "April 2"; // 2nd day of registration
+            const string DAY3 = "April 3"; // 3rd day of registration
+            const string DAY4 = "April 6"; // 4th day of registration
+            const string DAY5 = "April 7"; // 5th day of registration
+            const string DAY6 = "April 8"; // 6th day of registration
+
+            const string TIME1 = "8:30 AM";  // 1st time block
+            const string TIME2 = "10:00 AM"; // 2nd time block
+            const string TIME3 = "11:30 AM"; // 3rd time block
+            const string TIME4 = "2:00 PM";  // 4th time block
+            const string TIME5 = "4:00 PM";  // 5th time block
+
+            string lastNameStr;       // Entered last name
+            char lastNameLetterCh;    // First letter of last name, as char
+            string dateStr = "Error"; // Holds date of registration
+            string timeStr = "Error"; // Holds time of registration
+
+            lastNameStr = lastNameTxt.Text;
+
+            if (lastNameStr == "") // Empty text box
+                MessageBox.Show("Please enter last name!");
+            else
+            {
+                lastNameLetterCh = lastNameStr[0]; // As in text, p. 466-467
+
+                if (!char.IsLetter(lastNameLetterCh)) // Is it a letter or not?
+                    MessageBox.Show("Please ensure a letter is in first position of last name!");
+                else
+                {
+                    lastNameLetterCh = char.ToUpper(lastNameLetterCh); // Ensure upper case
+
+                    // Juniors and Seniors share same schedule but different days
+                    if (juniorBtn.Checked || seniorBtn.Checked)
+                    {
+                        if (seniorBtn.Checked)
+                            dateStr = DAY1;
+                        else // Must be juniors
+                            dateStr = DAY2;
+
+                        char[] JuniorSeniorLow = {'A', 'E', 'J', 'P', 'T'};
+                        string[] JuniorSeniorTimes = { TIME2, TIME3, TIME4, TIME5, TIME1 };
+
+                        int sub = JuniorSeniorLow.Length - 1;
+
+                        // Precondition: lastNameLetterCh < JuniorSeniorLow[sub] 
+                        // Post Condition: returns the time frame associated with the first letter of the users last name
+                        while (sub >= 0 && lastNameLetterCh < JuniorSeniorLow[sub])
+                            -- sub;
+                        timeStr = JuniorSeniorTimes[sub];
+                    }
+                    // Sophomores and Freshmen
+                    else // Must be soph/fresh
+                    {
+                        if (sophBtn.Checked)
+                        {
+                            // G-S on one day
+                            if ((lastNameLetterCh >= 'G') && // >= G and
+                                (lastNameLetterCh <= 'S'))   // <= S
+                                dateStr = DAY4;
+                            else // All other letters on previous day
+                                dateStr = DAY3;
+                        }
+                        else // must be freshman
+                        {
+                            // G-S on one day
+                            if ((lastNameLetterCh >= 'G') && // >= G and
+                                (lastNameLetterCh <= 'S'))   // <= S
+                                dateStr = DAY6;
+                            else // All other letters on previous day
+                                dateStr = DAY5;
+                        }
+
+                        char[] FreshmanSophomoreLow = { 'A', 'C', 'E', 'G', 'J', 'M', 'P', 'R','T','W' };
+                        string[] FreshmanSophomoreTimes = { TIME3, TIME4, TIME5, TIME1, TIME2, TIME3, TIME4, TIME5, TIME1, TIME2 };
+
+                        int sub1 = FreshmanSophomoreLow.Length - 1;
+                        // Pre Condition: lastNameLetterCh < FreshmanSophomoreLow[sub1] 
+                        // Post Condition: returns the time frame associated with the first letter of the users last name
+                        while (sub1 >= 0 && lastNameLetterCh < FreshmanSophomoreLow[sub1])
+                            --sub1;
+                        timeStr = FreshmanSophomoreTimes[sub1];
+                    }
+
+                    dateTimeLbl.Text = dateStr + " at " + timeStr;
+                }
+            }
+        }
+    }
+}
